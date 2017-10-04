02dd3c82fd3f4e0aa0a2c6721a8e260476d9b70b
Fraser Wilson <blancolioni@gmail.com>
2017-10-04 15:28:12 +0200

    CSS.Logging: log to file 'agama.log' in current directory (instead of to Standard_Output)

--- src/css-logging.adb
+++ src/css-logging.adb
@@ -2,15 +2,21 @@ with Ada.Text_IO;
 
 package body Css.Logging is
 
+   Log_File_Name : constant String := "agama.log";
+   Log_File      : Ada.Text_IO.File_Type;
+   Logging       : Boolean := False;
+
    ---------
    -- Log --
    ---------
 
    procedure Log (Message : String) is
    begin
-      if False then
-         Ada.Text_IO.Put_Line (Message);
+      if not Logging then
+         Ada.Text_IO.Create (Log_File, Ada.Text_IO.Out_File, Log_File_Name);
+         Logging := True;
       end if;
+      Ada.Text_IO.Put_Line (Log_File, Message);
    end Log;
 
    ---------
